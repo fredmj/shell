cb619684f27e3a2dc805a845b4f89f1b7b13629b
fredmj <fredericmariejoseph@gmail.com>
2014-09-22 17:36:54 +0200

    Change the storage location of a vob Make the primal structure.

---
+++ mv_stgloc.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#################################################################
+# FMJ Siemens 2014						#
+#								#
+#								#
+#								#
+#								#
+#								#
+#								#
+#								#
+#################################################################
+
+init() {
+ct=/opt/ibm/RationalSDLC/clearcase/bin/cleartool
+IFS="
+"
+AVB=$($ct lsstgloc -vob -long | grep 'Global path' | cut -d":" -f2 | sed -e's/ //g')
+HELP_MESSAGE="$0: Change the emplacement of the vob storage location.
+-h: this message
+-n|--new_vobstgloc_path : path to the new storage location
+"
+}
+
+control_vob-tag () {
+for vobs in $($ct lsvob)
+do
+echo "vob=$vobs"
+echo "ok pour VOB_TAG=${vobs%$AVB*}"
+echo "ok pour VOB_STG=$AVB${vobs##*$AVB}"
+echo "ok pour VOB_VBS=${vobs##*$AVB}"
+done
+}
+
+init
+
+while getopts hn: arg
+do
+case $arg in
+	h)
+	echo "$HELP_MESSAGE"
+	;;
+	n)
+	echo "this is the new path $OPTARG"
+	;;
+esac
+done
+
